7298ff3b9a918e80655bee20d78bd32daaf670d1
5qxs62t2v9-dot <5qxs62t2v9@privaterelay.appleid.com>
2026-04-20 14:20:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-const CACHE_NAME = 'pupok-fm-v4';
+const CACHE_NAME = 'pupok-fm-v5'; // ⬅️ версия увеличена для принудительного обновления кэша
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon.svg',
+  // иконки для iOS / PWA
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-180x180.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  // внешние ресурсы Font Awesome
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-regular-400.woff2',
